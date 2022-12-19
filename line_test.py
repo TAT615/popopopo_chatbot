f5615f3b6b8decb94f7ec05dcd7e17fb17c267df
TAT615 <itatuya15615@outlook.com>
2022-12-19 18:25:04 +0900

    Update line_test.py

--- line_test.py
+++ line_test.py
@@ -1,4 +1,5 @@
 import json
+import daterime
 file = open('info.json', 'r')
 info = json.load(file)
 
@@ -8,9 +9,11 @@ from linebot.models import TextSendMessage
 CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
+a = datetime.datetime.now()
+
 def main():
     USER_ID = info['USER_ID']
-    messages = TextSendMessage(text = "ぽぽぽぽ")
+    messages = TextSendMessage(text = "ぽぽぽぽ", a)
     line_bot_api.push_message(USER_ID, messages=messages)
 
 if __name__ == "__main__":
