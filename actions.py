f8e65cddc47cf1a2885d4e5536888aad4eaa084b
TAT615 <itatuya15615@outlook.com>
2022-12-19 18:10:07 +0900

    Update actions.py

--- actions.py
+++ actions.py
@@ -12,7 +12,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    messages = TextSendMessage(text = "ぽぽぽぽぽぽ")
+    messages = TextSendMessage(text = "`ぽっぽっぽ")
     line_bot_api.push_message(USER_ID, messages=messages)
 
 if __name__ == "__main__":
